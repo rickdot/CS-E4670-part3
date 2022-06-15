0151168848444ed898c37d2b5bf3bfe9a3705b95
yh <rickzen9x@gmail.com>
2022-06-15 23:44:03 +0300

    3.5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     { 
       "id": 1,
@@ -23,6 +25,17 @@ let persons = [
       "number": "39-23-6423122"
     }
 ]
+
+const generateId = () => {
+    // range 0-9999
+    let newID = Math.floor(Math.random() * 10000)
+    idarr = persons.map(n => n.id)
+    while (idarr.includes(newID)){
+      newID = Math.floor(Math.random() * 10000)
+    }
+    return newID
+}
+
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
@@ -57,6 +70,41 @@ app.get('/info', (request, response) => {
         `
         
     )
+})
+
+
+// 3.5
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name) {
+    return response.status(400).json({ 
+        error: 'name missing' 
+    })
+    }
+
+    if (!body.number) {
+    return response.status(400).json({ 
+        error: 'number missing' 
+    })
+    }
+
+    namearr = persons.map(n => n.name)
+    if (namearr.includes(body.name)){
+    return response.status(400).json({
+        error: 'name must be unique'
+    })
+    }
+    
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number,      
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
 })
 
 const PORT = 3001
